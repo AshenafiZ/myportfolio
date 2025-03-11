8b4f7beda2e9934e734dbad5ecc447804a291bf3
Ashenafi Zewdie <121452448+AshenafiZ@users.noreply.github.com>
2025-03-11 21:52:42 +0300

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <footer className="bg-black text-white py-6 px-4">
-      <div className="ml-20 max-w-6xl mx-auto flex flex-col md:flex-row justify-center items-center text-center md:text-left">
+      <div className="md:ml-20 max-w-6xl mx-auto flex flex-col md:flex-row justify-center items-center text-center md:text-left">
         <div className="mb-4 md:mb-0">
           <p className="text-sm">© {new Date().getFullYear()} Ashenafi Zewdie. All rights reserved.</p>
         </div>
